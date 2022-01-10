4b9a56be820762b3ff75a2283d7547b190835f4f
SantiagoLopez-hub <46728986+SantiagoLopez-hub@users.noreply.github.com>
2022-01-10 00:38:01 +0000

    Jumping forward character

--- sketch.js
+++ sketch.js
@@ -35,4 +35,37 @@ function draw()
     ellipse(gameChar_x - 8, gameChar_y - 49, 1, 3); // Left eye
     ellipse(gameChar_x + 8, gameChar_y - 49, 1, 3); // Right eye
     
+
+	//Jumping facing forwards
+	stroke(100);
+	noFill();
+	rect(220, 60, 50, 80);
+	noStroke();
+	fill(0);
+	text("2. jumping facing forwards", 220, 160);
+
+	gameChar_x = 245;
+	gameChar_y = 137;
+    
+    fill(232, 232, 232);
+    stroke(0);
+    rect(gameChar_x - 15, gameChar_y - 57, 30, 50, 3); // Body
+    
+    rect(gameChar_x - 13, gameChar_y - 7, 7, 5); // Left leg
+    rect(gameChar_x + 6, gameChar_y - 7, 7, 5); // Right leg
+    
+    rect(gameChar_x - 20, gameChar_y - 32, 5, -28); // Left arm
+    rect(gameChar_x + 15, gameChar_y - 32, 5, -28); // Right arm
+    
+    fill(175, 175, 175);
+    ellipse(gameChar_x, gameChar_y - 42, 5, 7); // Mouth
+    fill(0);
+    ellipse(gameChar_x - 8, gameChar_y - 49, 2, 3); // Left eye
+    ellipse(gameChar_x + 8, gameChar_y - 49, 2, 3); // Right eye
+    
+    fill(179, 167, 0);
+    ellipse(gameChar_x, gameChar_y - 67, 25, 5); // Outter Halo
+    fill(255);
+    ellipse(gameChar_x, gameChar_y - 67, 20, 5); // Inner Halo
+    
 }
